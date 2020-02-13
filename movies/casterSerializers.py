ca2c94c4e7960acc62e2cc6993e2e21aac17462d
Cradirow <hanorang2@naver.com>
2020-02-13 12:10:53 +0900

    delete some examples

--- movies/casterSerializers.py
+++
@@ -1,8 +0,0 @@
-
-from rest_framework import serializers
-from .models import Caster
-
-class CasterSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Caster
-		fields = ('name', 'date', 'did', 'do')
